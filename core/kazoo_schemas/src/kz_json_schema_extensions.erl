565383b8b6216a447dac070ebb468307e3f0616d
Harenson Henao <harenson@gmail.com>
2018-04-09 17:22:05 +0000

    KAZOO-5878: Add parameters stability level checks for crossbar request objects

--- core/kazoo_schemas/src/kz_json_schema_extensions.erl
+++ core/kazoo_schemas/src/kz_json_schema_extensions.erl
@@ -16,9 +16,10 @@
 -spec extra_validator(jesse:json_term(), jesse_state:state()) -> jesse_state:state().
 extra_validator(Value, State) ->
     Schema = jesse_state:get_current_schema(State),
+    State1 = maybe_check_params_stability_level(Value, Schema, State),
     case kz_json:is_true(<<"kazoo-validation">>, Schema, 'false') of
-        'true' -> extra_validation(Value, State);
-        'false' -> State
+        'true' -> extra_validation(Value, State1);
+        'false' -> State1
     end.
 
 -spec extra_validation(jesse:json_term(), jesse_state:state()) -> jesse_state:state().
@@ -115,3 +116,44 @@ validate_attachment_oauth_doc_id(Value, State) ->
             lager:debug("~s", [ErrorMsg]),
             jesse_error:handle_data_invalid('external_error', ErrorMsg, State)
     end.
+
+maybe_check_params_stability_level(Value, Schema, State) ->
+    case {kz_json:get_value(<<"properties">>, Schema),
+          kz_json:get_value(<<"id">>, Schema),
+          kz_json:is_json_object(Value)}of
+        {'undefined', _, _} ->
+            State; %% Skipping schema with out `properties' property
+        {Properties, Id, true} when Id /= <<"system_config.crossbar">> ->
+            check_params_stability_level(Properties, Value, State);
+        _ ->
+            State
+    end.
+
+check_params_stability_level(Properties, Value, State) ->
+    SystemSL = kapps_config:get_binary(<<"crossbar">>, <<"stability_level">>, <<"stable">>),
+    SystemSLInt = stability_level_to_int(SystemSL),
+    Fun = fun(K, V, Acc) -> check_param_stability_level(Properties, SystemSLInt, K, V, Acc) end,
+    case kz_json:foldl(Fun, [], Value) of
+        [] ->
+            State; %% Not invalid keys found
+        InvalidKeys ->
+            InvalidKeysBin = binary:list_to_bin(lists:join(<<", ">>, InvalidKeys)),
+            ErrorMsg = <<"Disallowed properties -they've lower stability level than"
+                         " system's stability level" " (", SystemSL/binary, ")-: ",
+                         InvalidKeysBin/binary>>,
+            lager:debug("~s", [ErrorMsg]),
+            jesse_error:handle_data_invalid('external_error', ErrorMsg, State)
+    end.
+
+check_param_stability_level(Properties, SystemSLInt, Key, _Val, Acc) ->
+    SearchKey = [Key, <<"stability_level">>],
+    PropSL = kz_json:get_value(SearchKey, Properties, <<"stable">>),
+    case stability_level_to_int(PropSL) < SystemSLInt of
+        true -> [Key | Acc];
+        false -> Acc
+    end.
+
+-spec stability_level_to_int(kz_term:ne_binary()) -> pos_integer().
+stability_level_to_int(<<"stable">>) -> 3;
+stability_level_to_int(<<"beta">>) -> 2;
+stability_level_to_int(<<"alpha">>) -> 1.
